13c972b40d06db302d3a46d0cc1a761fe92e84f5
mirror <mironosk@gmail.com>
2016-05-20 16:23:53 +0300

    add  new task

--- Memoizer.js
+++ Memoizer.js
@@ -40,28 +40,6 @@ function memorized (){
 	}
 }
 
-
-function memorized (){
-	var memory = [];
-	return function (fun,param){
-		if (memory[fun] && memory[fun][param] ){
-			console.log("buffered arg  is " +  param);
-			return memory[fun][param];
-		}else {
-			console.log("calculate arg  is"  + param);
-			if (! memory[fun]) {
-				memory[fun] = [param];
-			}else{
-				var params = memory[fun];
-				params.push(param);
-			}
-			memory[fun][param] = fun(param);
-			return memory[fun][param];
-		}		
-	}
-}
-
-
 function advancedMemorized (){
 	var memory = [];
 	return function (){
@@ -70,22 +48,24 @@ function advancedMemorized (){
 			if (typeof allArgs[0] == 'function') {
 				var args = [];
 				if (allArgs.length > 1) {
-					var args = allArgs.slice(1);
-					console.log("1")
+					var args = [];
+					args = allArgs.slice(1);
 				}
 				if (!memory[allArgs[0]]) {
-					console.log("2")
 					memory[allArgs[0]] = args;
 				}else{
-					console.log("3")
 					var params = memory[allArgs[0]];
 					params.push(args);
 				}
-				console.log(args);
-				console.log(allArgs);
-				console.log(memory);
+				console.log("all rgs----------- " + allArgs);
+				console.log('arg--------------- ' + args);
 
-				memory[allArgs[0]][args] = allArgs[0].apply(this,args);
+				console.log(memory);
+				console.log(allArgs[0].apply(this,args));
+
+
+				(memory[allArgs[0]][args]) = allArgs[0].apply(this,args);
+				console.log("in memory------- " + memory[allArgs[0]]);
 				return memory[allArgs[0]][args];
 			};
 		};		
@@ -93,4 +73,4 @@ function advancedMemorized (){
 }
 
 var b = advancedMemorized();
-console.log(b(factorial,5));
+console.log(b(factorial,3));
